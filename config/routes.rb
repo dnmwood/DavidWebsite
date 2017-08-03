8524b9779dd61167553ddaac99a74d30027e7b99
DAVID <davidnmwood@gmail.com>
2017-08-03 14:47:29 -0400

    setup resources, add root index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects, only: [:index, :show]
+  root 'projects#index'
   
-  get 'projects/index'
-
-  get 'projects/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
