8605d40954ae26bd785149c61a1bae620201328c
DAVID <davidnmwood@gmail.com>
2017-08-04 15:10:07 -0400

    Setup basic Project controller functions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,39 @@
 class ProjectsController < ApplicationController
+
   def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+      if @project.save
+        redirect_to project_url
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+      if @project.destroy
+        redirect_to project_url
+      end
+  end
+
+private
+  def project_params
+    params.require(:project).permit(:title, :description, :image)
   end
 end
